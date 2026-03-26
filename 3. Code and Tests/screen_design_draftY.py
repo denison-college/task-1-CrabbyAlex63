8deb71eedabce6b86783c924c40bf057adb86813
Alexander Cottier <alexander.cottier@education.nsw.gov.au>
2026-03-26 14:42:17 +1100

    fixed speed

--- 3. Code and Tests/screen_design_draftY.py
+++ 3. Code and Tests/screen_design_draftY.py
@@ -21,7 +21,7 @@ def main():
     parser.add_argument(
         'action', 
         metavar='Main Menu',
-        choices=['Play', 'Instructions', 'Settings', 'Exit'],
+        choices=['Play', 'Instructions'],
         help='What would you like to do?'
     )
     
@@ -29,7 +29,6 @@ def main():
         'difficulty',
         metavar='Difficulty',
         choices=['Easy', 'Medium', 'Hard'],
-        default='Medium',
         help='Choose your challenge level'
     )
 
@@ -40,17 +39,17 @@ def main():
     
     # Play action
     if args.action == "Play":
-        slow_print("Nice Choice!")
-        slow_print(f"Starting game on {args.difficulty} mode...")
+        print("Nice Choice!")
+        print(f"Starting game on {args.difficulty} mode...")
         
 
     # Instructions action
     elif args.action == "Instructions":
-        slow_print("Knowledge means survival.")
-        slow_print("The difficulty you choose will dictate how hard and more bomb-packed the game is. Your goal is to strategically locate and mark all the bombs without setting one off. Upon your mission, you will come across ‘powers’ which may be beneficial or negative towards your mission. To win the game, all tiles must be correctly marked or cleared. To mark a tile of a bomb, you are to type the coordinates of the chosen tile, then when given the selection prompt, you choose ‘Mark’. This will change the tile into a ‘p’, this represents a flag. To reveal a tile, you are to type the coordinates of the chosen tile, then when given the selection prompt, you choose ‘Reveal’. This will change the tile to one of the following 2: 0, which represents a clear tile, or an ?, which represents a bomb. Upon discovering a bomb you will lose the game.")
+        print("Knowledge means survival.")
+        print("The difficulty you choose will dictate how hard and more bomb-packed the game is. Your goal is to strategically locate and mark all the bombs without setting one off. Upon your mission, you will come across ‘powers’ which may be beneficial or negative towards your mission. To win the game, all tiles must be correctly marked or cleared. To mark a tile of a bomb, you are to type the coordinates of the chosen tile, then when given the selection prompt, you choose ‘Mark’. This will change the tile into a ‘p’, this represents a flag. To reveal a tile, you are to type the coordinates of the chosen tile, then when given the selection prompt, you choose ‘Reveal’. This will change the tile to one of the following 2: 0, which represents a clear tile, or an ?, which represents a bomb. Upon discovering a bomb you will lose the game.")
 
     else:
-        slow_print(f"You selected: {args.action}")
+        print(f"You selected: {args.action}")
  
     
 # Gooey LAUNCHER
